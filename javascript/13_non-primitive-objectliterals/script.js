547013535dc4b7e9c193cf158dc63a62f48eed7a
suraj verma <surajverma.off@gmail.com>
2026-07-29 22:54:13 +0530

    object literals

---
+++ javascript/13_non-primitive-objectliterals/script.js
@@ -0,0 +1,10 @@
+const user1={name:'suraj',age:25,address:"giridih",pata:{
+    pinCode:825647,dhanbadPincode:828304
+},
+"user-phone":7785566586
+}
+console.log(user1.pata.pinCode);
+console.log(user1["user-phone"]);
+console.log(user1.name);
+
+    
