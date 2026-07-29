caec955c8a58638c8e0466d9730d1aded7a60a22
suraj verma <surajverma.off@gmail.com>
2026-07-29 22:56:16 +0530

    print age

--- javascript/13_non-primitive-objectliterals/script.js
+++ javascript/13_non-primitive-objectliterals/script.js
@@ -6,5 +6,6 @@ const user1={name:'suraj',age:25,address:"giridih",pata:{
 console.log(user1.pata.pinCode);
 console.log(user1["user-phone"]);
 console.log(user1.name);
+console.log(user1.age);
 
     
