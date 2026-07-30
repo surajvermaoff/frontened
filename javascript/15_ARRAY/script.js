145887707e577c3d893d4eda82e92a80bab7eb77
suraj verma <surajverma.off@gmail.com>
2026-07-30 22:18:37 +0530

    array

---
+++ javascript/15_ARRAY/script.js
@@ -0,0 +1,6 @@
+const rainbow =["violet " ,"indigo","blue","green","yellow","orange","red"]
+console.log(rainbow[3])
+rainbow.push("black")
+rainbow.pop()
+console.log(rainbow);
+console.log(rainbow.length);
