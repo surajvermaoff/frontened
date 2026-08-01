28be24b64ad54036d2bc557e93991eafa832b7c3
suraj verma <surajverma.off@gmail.com>
2026-08-01 23:57:10 +0530

    array methods

---
+++ javascript/16_arraymethods/script.js
@@ -0,0 +1,13 @@
+const numbers= [1,2,3,4,5,6,7]
+const numbers2 = [8,9]
+numbers.push(0)
+numbers.pop()
+const totlNumbers= numbers.concat(numbers2)
+numbers.shift(1)
+numbers.unshift(1)
+numbers.indexOf(5)
+numbers.includes(6)
+numbers.sort()
+totalNumbers.reverse()
+numbers.slice(2,4)//gives a new array with the elements passed inside 
+numbers.splice(2,2,"gdghggxc") //gives and modifies the array actually removes and add elements instead of passed elements
